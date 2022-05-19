9f8f3d35e65cb2be37fdaf92dad6c67f4bec31e3
AlbertiPot <1071794833@qq.com>
2022-05-19 07:54:18 +0000

    final submit

--- launch.sh
+++ launch.sh
@@ -1,7 +1,6 @@
 # train
-CUDA_VISIBLE_DEVICES=3 python main.py \
---save_name 'train_final'
-
+# CUDA_VISIBLE_DEVICES=3 python main.py \
+# --save_name 'train_final'
 # search
-# CUDA_VISIBLE_DEVICES=3 python test.py \
-# --save_name 'test_final'
+CUDA_VISIBLE_DEVICES=3 python test.py \
+--save_name 'test_final'
